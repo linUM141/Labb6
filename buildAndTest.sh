6b8d7cbf5215584ce0a9051c4c040f28220b6edf
emilw <emil@postback.se>
2017-02-16 07:50:00 +0100

    Added change to build script to make sure that the whole script fails when a test is not passing

--- buildAndTest.sh
+++ buildAndTest.sh
@@ -1,21 +1,21 @@
 #Test resistance lib
-echo Running tests of libresistance
-make -C lib/libresistance test
+echo "Running tests of libresistance" && \
+make -C lib/libresistance test && \
 
 #Test component lib
-echo Running tests of libcomponent
-make -C lib/libcomponent test
+echo "Running tests of libcomponent" && \
+make -C lib/libcomponent test && \
 
 #Test full program
-echo Running regular make
-make
+echo "Running regular make" && \
+make make && \
 
-echo Running make lib
-make lib
+echo "Running make lib make" && \
+make lib make && \
 
-echo Testing installation
-make install
+echo "Testing installation make" && \
+make install make && \
 
-echo Testing uninstall
-make uninstall
+echo "Testing uninstall make" && \
+make uninstall make
 
